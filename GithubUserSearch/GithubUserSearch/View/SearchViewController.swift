d95c2005e08d9058a559d37a05d36160f4c45ea2
KayAhn0126 <dkseotn123@gmail.com>
2022-09-20 20:41:33 +0900

    refactor: SearchController 추가

--- GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -13,5 +13,37 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createSearchController()
+    }
+    
+    private func createSearchController() {
+        self.navigationItem.title = "Search"
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = true
+        searchController.searchBar.placeholder = "Search Github User..."
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let text = searchController.searchBar.text
+        print("\(text)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("completed with : \(searchBar.text)")
+        searchBar.resignFirstResponder()
+    }
+}
+
+extension SearchViewController: UISearchControllerDelegate {
+    func didPresentSearchController(_ searchController: UISearchController) {
+        searchController.searchBar.becomeFirstResponder()
     }
 }
