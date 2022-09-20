908ecefaad67ef243226fb7ab04397273ab5feb1
KayAhn0126 <dkseotn123@gmail.com>
2022-09-20 17:07:21 +0900

    feat: GithubUserSearch 프로젝트 추가

---
+++ GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GithubUserSearch
+//
+//
+
+import UIKit
+import Combine
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
